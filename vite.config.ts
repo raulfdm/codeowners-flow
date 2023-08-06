d120542920f50b825f1f40e2a9f7e748797aff32
Raul Melo <melo.raulf@gmail.com>
2023-08-06 09:54:09 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
 import { defineConfig } from "vite";
+import path from "node:path";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
   plugins: [dts()],
   build: {
-    lib: {
-      entry: {
-        index: "src/index.ts",
-        config: "src/config.ts",
+    target: "node16",
+    rollupOptions: {
+      external: function isExternal(id: string) {
+        return !id.startsWith(".") && !path.isAbsolute(id);
       },
-      formats: ["cjs"],
     },
     outDir: "dist",
+    lib: {
+      entry: {
+        ci: "src/cli.ts",
+        config: "src/config.ts",
+      },
+      formats: ["cjs", "es"],
+    },
   },
 });
