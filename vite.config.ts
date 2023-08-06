bbe83100cf162a807d15e1a07d9ca91b32608182
Raul Melo <melo.raulf@gmail.com>
2023-08-06 08:45:16 +0200

    add submodule config and generate types

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [dts()],
   build: {
     lib: {
-      entry: "src/index.ts",
-      formats: ["es"],
+      entry: {
+        index: "src/index.ts",
+        config: "src/config.ts",
+      },
+      formats: ["cjs"],
     },
     outDir: "dist",
   },
