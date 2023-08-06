48bc98ff727bf3e8d3f69c62965f81e2e4612ed5
Raul Melo <melo.raulf@gmail.com>
2023-08-06 09:55:16 +0200

    Implement basic config (#2)
    closes #1

--- codeowners.config.mjs
+++ codeowners.config.mjs
@@ -1,7 +1,20 @@
-export default [
-  {
-    path: "*",
-    owners: ["@owner1", "@owner2"],
-    comments: [],
-  },
-];
+const mainTeams = ["@company/core-team", "@company/infra-team"];
+
+/** @type {import('./dist/src/config.d.ts').UserConfig} */
+export default {
+  outDir: ".github",
+  rules: [
+    {
+      match: "*",
+      owners: mainTeams,
+      comments: [
+        "Everything else will be fallback to @company/core-team to approve",
+      ],
+    },
+    {
+      match: "apps/website-frontend",
+      excludeMatch: ["apps/website-frontend/github"],
+      owners: ["@company/website-team", ...mainTeams],
+    },
+  ],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from "vite";
+import path from "node:path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [dts()],
   build: {
-    lib: {
-      entry: "src/index.ts",
-      formats: ["es"],
+    target: "node16",
+    rollupOptions: {
+      external: function isExternal(id: string) {
+        return !id.startsWith(".") && !path.isAbsolute(id);
+      },
     },
     outDir: "dist",
+    lib: {
+      entry: {
+        ci: "src/cli.ts",
+        config: "src/config.ts",
+      },
+      formats: ["cjs", "es"],
+    },
   },
 });
