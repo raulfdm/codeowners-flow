fccbbe71103479c0f303c01f8c33865a0d28f9ec
Raul Melo <melo.raulf@gmail.com>
2023-08-13 10:52:08 +0200

    add unit tests to code to generate codeowners

---
+++ packages/cli/src/generate-codeowners.test.ts
@@ -0,0 +1,82 @@
+import type { UserConfig } from "@codeowners-js/config";
+import { generateCodeOwners } from "./generate-codeowners.js";
+
+const mockLoadConfig = vi.fn();
+vi.mock("@codeowners-js/config", () => ({
+  loadConfig: (...args: any) => mockLoadConfig(...args),
+}));
+
+const mockWriteFileSync = vi.fn();
+vi.mock("node:fs", () => ({
+  default: {
+    writeFileSync: (...args: any) => mockWriteFileSync(...args),
+  },
+}));
+
+const mockFindRoot = vi.fn();
+vi.mock("@manypkg/find-root", () => ({
+  findRoot: (...args: any) => mockFindRoot(...args),
+}));
+
+describe("fn: generateCodeOwners", () => {
+  beforeEach(() => {
+    mockLoadConfig.mockResolvedValue({
+      outDir: "./test",
+      rules: [
+        {
+          owners: [
+            {
+              name: "@team/core",
+            },
+          ],
+          patterns: ["packages/core/**/*"],
+        },
+      ],
+    } satisfies UserConfig);
+
+    mockFindRoot.mockResolvedValue({
+      rootDir: "/root",
+    });
+  });
+
+  it("writes CODEOWNERS file in the expected location", async () => {
+    await generateCodeOwners();
+
+    const [location] = mockWriteFileSync.mock.calls[0];
+    expect(location).toBe("/root/test/CODEOWNERS");
+  });
+
+  it.concurrent("writes CODEOWNERS content", async () => {
+    await generateCodeOwners();
+
+    const [_, content] = mockWriteFileSync.mock.calls[0];
+    expect(content).toMatchInlineSnapshot(`
+      "# This file was generated automatically by codeowners-js. Do not edit it manually.
+      # Instead, change the \`codeowners.config.mjs\` file in the root of your project.
+
+      # -------------------- START -------------------- #
+      ## Matching patterns...
+      packages/core/**/* @team/core
+      # -------------------- END -------------------- #
+      "
+    `);
+  });
+
+  it("returns codeowners path and content", async () => {
+    const result = await generateCodeOwners();
+
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "ownersContent": "# This file was generated automatically by codeowners-js. Do not edit it manually.
+      # Instead, change the \`codeowners.config.mjs\` file in the root of your project.
+
+      # -------------------- START -------------------- #
+      ## Matching patterns...
+      packages/core/**/* @team/core
+      # -------------------- END -------------------- #
+      ",
+        "ownersPath": "/root/test/CODEOWNERS",
+      }
+    `);
+  });
+});
