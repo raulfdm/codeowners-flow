654dc7ad8d8221fe599955d908d1fc259f29957e
Raul Melo <melo.raulf@gmail.com>
2025-04-07 06:50:04 +0200

    add fixture

---
+++ packages/cli/src/__fixtures__/codeowners.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  outDir: '.github',
+  rules: [
+    {
+      patterns: ['*'],
+      owners: [
+        {
+          name: '@company-team',
+        },
+      ],
+    },
+  ],
+};
